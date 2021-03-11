d694bffe4f1479f3258f70c3e8a5dfb8cb34f7e3
lunsong.xia <lunsong.xia@i9i8.com>
2021-03-11 14:45:01 +0800

    ls-rpc项目 启动

---
+++ lsrpc-core/src/main/java/com/github/GenericService.java
@@ -0,0 +1,28 @@
+package com.github;
+
+import com.sun.xml.internal.ws.util.CompletedFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>date : 2021/3/11 14:19</p>
+ * description: 泛化调用接口，由于没有目标类，复杂参数对象采用Map进行传输，泛型调用不会出现Callback
+ *
+ * @author : crazy_lunsong
+ * @version : 1.0
+ */
+public interface GenericService {
+    default Object invoke(String method, String[] parameterTypes, Object[] args) {
+        try {
+            async(method, parameterTypes, args).get(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (ExecutionException e) {
+            Throwable throwable = e.getCause() == null ? e : e.getCause();
+            if (throwable instanceof) {
+
+            }
+        }
+    }
+
+    CompletedFuture<?> async(String method, String[] parameterTypes, Object[] args);
+}
